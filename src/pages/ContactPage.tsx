a6da312cd7e7dda99cb3553a5185b689b8d36889
thaheshan suresh <166691277+thaheshan@users.noreply.github.com>
2025-05-26 13:20:12 +0530

    updated cintact page
    updated contact page with correct map

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -170,14 +170,16 @@ const ContactPage: React.FC = () => {
       {/* Map */}
       <div className="mt-12">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.9024424301397!2d90.39108661498136!3d23.750858084589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8983f79fa27%3A0xeddafd73d038bc2f!2sBijoy%20Sarani%2C%20Dhaka%2C%20Bangladesh!5e0!3m2!1sen!2sus!4v1645488945876!5m2!1sen!2sus"
-          width="100%"
-          height="450"
-          style={{ border: 0 }}
-          allowFullScreen
-          loading="lazy"
-          className="rounded-lg shadow-md"
-        ></iframe>
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.138481592157!2d81.23088547593789!3d8.566811997191498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afbbde48f278b21%3A0xf1fca4008c381ec7!2sSmart%20Touch%20PC!5e0!3m2!1sen!2slk!4v1716729863507!5m2!1sen!2slk"
+  width="100%"
+  height="450"
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+  className="rounded-lg shadow-md"
+/>
+
       </div>
     </div>
   );
